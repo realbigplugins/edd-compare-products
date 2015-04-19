c195abe0ec9f6397b81c4addd1cdd45ef08d5c20
Kyle Maurer <kyle@realbigmarketing.com>
2015-04-19 11:43:30 -0400

    I'm getting good at this. Added more javascript to get all the go buttons synced up and going to the right place. YES!!!

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,4 +4,13 @@
 function eddCompareURL(id) {
     var url = "edd-compare-url";
     document.getElementById(url).innerText = document.getElementById(url).innerText + id + ',';
+    document.getElementById('edd-compare-button-' + id).style.display = 'none';
+    document.getElementById('edd-compare-go-button-' + id).style.display = 'block';
+    /* instead of this, I need to get all buttons with edd-compare-go-button- in the ID and update each
+    every time this function runs.
+     */
+    var elements = document.getElementsByClassName("edd-compare-go");
+    for(var i=0; i<elements.length; i++) {
+        elements[i].setAttribute("href", document.getElementById(url).innerText);
+    }
 }
--- includes/functions.php
+++ includes/functions.php
@@ -10,8 +10,15 @@ if( !defined( 'ABSPATH' ) ) exit;
 
 function edd_compare_products_add_compare_button( $purchase_form, $args ) {
 
+	global $edd_options;
+	$page = $edd_options['edd-compare-products-page'];
+	if ( ! $page ) {
+		return $purchase_form;
+	}
+
 	$class = implode( ' ', array( $args['style'], $args['color'], trim( $args['class'] ) ) );
-	$button = '<button class="edd-submit button ' . esc_attr( $class ) . '" onclick="eddCompareURL(' . $args["download_id"] . ')">Compare</button>';
+	$button = '<button class="edd-submit button ' . esc_attr( $class ) . '" id="edd-compare-button-' . $args["download_id"] . '" onclick="eddCompareURL(' . $args["download_id"] . ')">Compare</button>';
+	$button = $button . '<a class="edd-submit edd-compare-go button ' . esc_attr( $class ) . '" href="" id="edd-compare-go-button-' . $args["download_id"] . '" style="display:none">Go to Comparison</a>';
 	return $purchase_form . $button;
 }
 
